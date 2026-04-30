35730a5f6c9144eb1f8990bfac7ef4e584e95a60
polovinka.heart@gmail.com <polovinka.heart@gmail.com>
2026-04-30 22:51:05 +0300

    the first comment

---
+++ src/masks.py
@@ -0,0 +1,29 @@
+def get_mask_card_number(card_number: str) -> str:
+    """ Функция маскирует номер карты"""
+    list_numbers = []
+    for i in range(0, len(card_number), 4):
+        numbers = card_number[i : i + 4]
+        list_numbers.append(numbers)
+    if len(list_numbers) >= 4:
+        list_numbers[1] = list_numbers[1][:2] + "**"
+        list_numbers[2] = "****"
+    return " ".join(list_numbers)
+
+
+card_number = input("Введите номер карты: ")
+print(get_mask_card_number(card_number))
+
+
+def get_mask_account(score: str) -> str:
+    """ Функция маскирует номер счета"""
+    list_score = []
+    for i in range(0, len(score), 4):
+        numbers = score[i : i + 4]
+        list_score.append(numbers)
+    if len(list_score) >= 4:
+        result_score = "**" + list_score[-1]
+    return "".join(result_score)
+
+
+score = input("Введите номер карты: ")
+print(get_mask_account(score))
