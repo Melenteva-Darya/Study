b28f775c16423b7ef5aa3fd12c12bda55a23ec19
Darya Melenteva <d.v.melenteva@gmail.com>
2026-05-10 23:09:30 +0300

    проверено на соответствие линтерам

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,9 @@
-def filter_by_state(list_state: list[dict], state: str = "EXECUTED") -> list[dict]:
+from typing import Any
+
+
+def filter_by_state(list_state: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
     """Функция выводит список словарей, содержащие только те словари, у которых ключ
-state соответствует указанному значению."""
+    state соответствует указанному значению."""
     result_state = []
     for dictionary in list_state:
         if dictionary["state"] == state:
@@ -8,7 +11,7 @@ state соответствует указанному значению."""
     return result_state
 
 
-def sort_by_date(list_data: list[dict], reverse=True) -> list[dict]:
+def sort_by_date(list_data: list[dict[str, Any]], reverse=True) -> list[dict[str, Any]]:
     """Функция сортирует список по дате"""
     result_data = sorted(list_data, key=lambda x: x["date"], reverse=reverse)
     return result_data
