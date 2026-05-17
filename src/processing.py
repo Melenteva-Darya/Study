1dbfa9cc6a41817d308cbf692bd19e92da7f1090
Darya Melenteva <d.v.melenteva@gmail.com>
2026-05-17 15:24:03 +0300

    промежуточный

--- src/processing.py
+++ src/processing.py
@@ -19,7 +19,20 @@ def filter_by_state(list_state: list[dict[str, Any]], state: str = "EXECUTED") -
     return result_state
 
 
-def sort_by_date(list_data: list[dict[str, Any]], reverse=True) -> list[dict[str, Any]]:
+def sort_by_date(list_data: list[dict[str, Any]], reverse=True) -> Any:
     """Функция сортирует список по дате"""
+
+    if not list_data:
+        return "Ошибка: список пуст!"
+
+    if reverse == "":
+        reverse = True
+
     result_data = sorted(list_data, key=lambda x: x["date"], reverse=reverse)
     return result_data
+
+
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-06-30T21:27:25.241689'},
+       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -14,6 +14,7 @@ def test_filter(input_argument_list, state_argument, output_function):
 
 
 def test_filter_empty():
+
     input_argument_list = []
     state_argument = 'CANCELED'
     output_function = "Ошибка: список пуст!"
@@ -28,3 +29,35 @@ def test_filter_empty_state(input_argument_list):
                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
 
     assert filter_by_state(input_argument_list, state_argument) == output_function
+
+
+@pytest.mark.parametrize("reverse_argument, output_function",
+                         [(True, [{'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
+                          (False, [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                                   {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                   {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                   {'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, ]),
+                          ('', [{'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
+                          ])
+def test_sort(input_argument_list, reverse_argument, output_function):
+    assert sort_by_date(input_argument_list, reverse_argument) == output_function
+
+
+def test_sort_identical_dates():
+    input_argument_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-06-30T21:27:25.241689'},
+                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    reverse_argument = True
+    output_function = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-06-30T21:27:25.241689'},
+                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+    assert sort_by_date(input_argument_list, reverse_argument) == output_function
