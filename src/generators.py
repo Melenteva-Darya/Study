14ac2c31c45eb7e1209f0095d2021a06d910859a
Darya Melenteva <d.v.melenteva@gmail.com>
2026-05-24 15:55:30 +0300

    end home work 11_1

--- src/generators.py
+++ src/generators.py
@@ -1,29 +1,31 @@
-from mypyc.primitives.misc_ops import yield_from_except_op
+from typing import Any, Generator
 
 
-def filter_by_currency(transactions, currency=""):
+def filter_by_currency(transactions: list[dict[str, Any]], currency: str = "") -> Generator[dict[str, Any], Any, None]:
     """Функция выводит список словарей, содержащие только те транзакции,
-       где валюта операции соответствует заданной."""
+    где валюта операции соответствует заданной."""
     if currency == "":
         raise ValueError("Необходимо указать валюту для поиска.")
 
-    result_currency = (dictionary for dictionary in transactions if
-                       dictionary.get("operationAmount", {}).get("currency", {}).get("code") == currency)
+    result_currency = (
+        dictionary
+        for dictionary in transactions
+        if dictionary.get("operationAmount", {}).get("currency", {}).get("code") == currency
+    )
     return result_currency
 
 
-def transaction_descriptions(transactions):
+def transaction_descriptions(transactions: list[dict[str, Any]]) -> Generator[str, None, None]:
     """Функция-генератор, которая принимает список словарей и
     поочередно возвращает описание каждой операции."""
     for operation in transactions:
         yield operation.get("description", "")
 
 
-def card_number_generator(start, stop):
+def card_number_generator(start: int, stop: int) -> Generator[str, int, None]:
     """Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX
-       в заданном диапазоне от start до stop включительно."""
+    в заданном диапазоне от start до stop включительно."""
     for number in range(start, stop + 1):
         str_number = f"{number:016d}"
         number_card = f"{str_number[:4]} {str_number[4:8]} {str_number[8:12]} {str_number[12:16]}"
         yield number_card
-
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -5,17 +5,21 @@ import pytest
 from src.widget import get_date, mask_account_card
 
 
-@pytest.mark.parametrize("input_argument, output_function",
-                         [("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
-                          ("Счет 73654108430135874305", "Счет **4305"),
-                          ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
-                          ("Счет 64686473678894779589", "Счет **9589"),
-                          ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
-                          ("Счет 35383033474447895560", "Счет **5560"),
-                          ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
-                          ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
-                          ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
-                          ("Счет 73654108430135874305", "Счет **4305"), ])
+@pytest.mark.parametrize(
+    "input_argument, output_function",
+    [
+        ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("Счет 64686473678894779589", "Счет **9589"),
+        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+        ("Счет 35383033474447895560", "Счет **5560"),
+        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
+        ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+        ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+    ],
+)
 def test_mask_1(input_argument: Any, output_function: Any) -> None:
     assert mask_account_card(input_argument) == output_function
 
@@ -36,13 +40,16 @@ def test_mask_5() -> None:
     assert mask_account_card("Visa #@g") == "Ошибка: строка содержит недопустимые символы!"
 
 
-@pytest.mark.parametrize("input_argument, output_function",
-                         [("2024-03-11T02:26:18.671407", "11.03.2024"),
-                          ("  2024-03-12T02:26:18.671407  ", "12.03.2024"),
-                          ("Перевод организации 2024-03-13T02:26:18.671407", "13.03.2024"),
-                          ("", "Ошибка: пустая строка!"),
-                          ("2024-03-11-T02:26:18.671407", "Ошибка: неверный формат даты!"),
-                          ("20224-03-11-T02:26:18.671407", "Ошибка: неверный формат даты!"),
-                          ])
+@pytest.mark.parametrize(
+    "input_argument, output_function",
+    [
+        ("2024-03-11T02:26:18.671407", "11.03.2024"),
+        ("  2024-03-12T02:26:18.671407  ", "12.03.2024"),
+        ("Перевод организации 2024-03-13T02:26:18.671407", "13.03.2024"),
+        ("", "Ошибка: пустая строка!"),
+        ("2024-03-11-T02:26:18.671407", "Ошибка: неверный формат даты!"),
+        ("20224-03-11-T02:26:18.671407", "Ошибка: неверный формат даты!"),
+    ],
+)
 def test_data_1(input_argument: Any, output_function: Any) -> None:
     assert get_date(input_argument) == output_function
