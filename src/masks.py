8a6d066ffd1666320c018ea8b55c95e75bcefbda
Darya Melenteva <d.v.melenteva@gmail.com>
2026-06-09 21:22:00 +0300

    finish

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,5 @@
-import os
 import logging
+import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 log_dir = os.path.abspath(os.path.join(script_dir, "..", "logs"))
@@ -7,18 +7,20 @@ log_file_path = os.path.join(log_dir, "masks.log")
 
 utils_logger = logging.getLogger(__name__)
 utils_logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler("logs/masks.log")
+file_handler = logging.FileHandler(log_file_path, mode="w", encoding="utf-8")
 
-file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 
 utils_logger.addHandler(file_handler)
-utils_logger.debug('Debug message')
+utils_logger.debug("Debug message")
 
 
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскирует номер карты"""
     list_numbers = []
+    utils_logger.info("Программа успешно запущена")
+
     if len(card_number) == 16:
         for i in range(0, len(card_number), 4):
             numbers = card_number[i : i + 4]
@@ -26,10 +28,16 @@ def get_mask_card_number(card_number: str) -> str:
         if len(list_numbers) >= 4:
             list_numbers[1] = list_numbers[1][:2] + "**"
             list_numbers[2] = "****"
+
     elif not card_number:
-        return "Ошибка: пустая строка!"
+        utils_logger.warning("Ошибка: пустая строка")
+        return "Внимание пустая строка!"
+
     else:
-        return "Ошибка: неверное количество цифр (нужно 16)!"
+        utils_logger.warning("Ошибка: неверное количество цифр")
+        return "Внимание: неверное количество цифр (нужно 16)!"
+
+    utils_logger.info("Программа успешно завершена")
     return " ".join(list_numbers)
 
 
@@ -37,14 +45,19 @@ def get_mask_account(score: str) -> str:
     """Функция маскирует номер счета"""
     result_score = ""
     list_score = []
+
+    utils_logger.info("Программа успешно запущена")
     if len(score) == 20:
         for i in range(0, len(score), 4):
             numbers = score[i : i + 4]
             list_score.append(numbers)
         if len(list_score) >= 4:
             result_score = "**" + list_score[-1]
+        utils_logger.info("Программа успешно завершена")
         return "".join(result_score)
     elif not score:
-        return "Ошибка: пустая строка!"
+        utils_logger.warning("Ошибка: пустая строка")
+        return "Внимание пустая строка!"
     else:
-        return "Ошибка: неверное количество цифр (нужно 20)!"
+        utils_logger.error("Ошибка: неверное количество цифр (нужно 20)!")
+        return "Внимание: неверное количество цифр (нужно 20)!"
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,8 @@
 import json
-import os
 import logging
+import os
 from json import JSONDecodeError
 from typing import Any, Union
-
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 path_to_file = os.path.abspath(os.path.join(script_dir, "..", "data", "operations.json"))
@@ -14,24 +13,25 @@ log_file_path = os.path.join(log_dir, "utils.log")
 
 utils_logger = logging.getLogger(__name__)
 utils_logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler("logs/utils.log")
+file_handler = logging.FileHandler(log_file_path, mode="w", encoding="utf-8")
 
-file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 
 utils_logger.addHandler(file_handler)
-utils_logger.debug('Debug message')
+utils_logger.debug("Debug message")
+
 
 def get_financial_transactions(file_path: str) -> Union[list[Any], str]:
     """Читает JSON-файл и возвращает список словарей с финансовыми транзакциями.
     Если файл не найден, пуст или содержит не список — возвращает []."""
     try:
         if not file_path:
-            utils_logger.info(f'Путь к файлу не указан')
+            utils_logger.warning("Путь к файлу не указан")
             return []
 
         if not os.path.exists(file_path):
-            utils_logger.info(f'Файл не найден')
+            utils_logger.warning("Файл не найден")
             return []
 
         utils_logger.info(f"Начало чтения данных из файла: {file_path}")
@@ -39,11 +39,9 @@ def get_financial_transactions(file_path: str) -> Union[list[Any], str]:
         with open(file_path, "r", encoding="utf-8") as f:
             data = json.load(f)
             if isinstance(data, list):
-                utils_logger.info(
-                    f"Данные успешно прочитаны. Найдено транзакций: {len(data)}"
-                )
+                utils_logger.info(f"Данные успешно прочитаны. Найдено транзакций: {len(data)}")
                 return data
             return []
     except (JSONDecodeError, PermissionError, FileNotFoundError) as e:
-        utils_logger.error(f'Произошла ошибка: {e}', exc_info=True)
+        utils_logger.error(f"Произошла ошибка: {e}", exc_info=True)
         return []
